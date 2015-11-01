8ca4bdd02905a91f9517638542c504dfc0b359b5
Thupten Chakrishar <vajradog@gmail.com>
2015-11-01 10:31:20 -0500

    go to index after created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to @post, notice: 'Post was successfully created.'
+      redirect_to @posts, notice: 'Post was successfully created.'
     else
       render :new
     end
